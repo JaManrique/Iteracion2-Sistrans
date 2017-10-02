08293d4d5d66c545446b324e917c92d771d8c586
Santiago Rodriguez Matiz <s.rodriguezm@uniandes.edu.co>
2017-10-02 01:49:40 -0500

    zonas

--- src/vos/Zona.java
+++ src/vos/Zona.java
@@ -1,5 +1,34 @@
 package vos;
 
-public class Zona {
+import org.codehaus.jackson.annotate.JsonProperty;
 
+public class Zona {
+	@JsonProperty(value="nombre")
+	private String nombre;
+	
+	@JsonProperty(value="capacidad")
+	private Integer capacidad;
+	
+	public Zona(@JsonProperty(value="nombre")String pNom,@JsonProperty(value="capacidad")Integer ctrn) {
+		super();
+		nombre=pNom;
+		capacidad=ctrn;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Integer getCapacidad() {
+		return capacidad;
+	}
+
+	public void setCapacidad(Integer capacidad) {
+		this.capacidad = capacidad;
+	}
+	
 }
