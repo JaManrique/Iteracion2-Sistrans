7a5f3c086a53dd45967e69ea65ef258d920a2ed7
Santiago Rodriguez Matiz <s.rodriguezm@uniandes.edu.co>
2017-10-01 23:18:53 -0500

    DAOS PRDUCTO Y RESTAURANTE(REQS)

--- src/vos/Producto.java
+++ src/vos/Producto.java
@@ -1,5 +1,86 @@
 package vos;
 
+import org.codehaus.jackson.annotate.JsonProperty;
+
+import com.sun.javafx.iio.png.PNGIDATChunkInputStream;
+
 public class Producto {
+	
+	@JsonProperty(value="nombre")
+	private String nombre;
+	
+	@JsonProperty(value="categoria")
+	private Integer categoria;
+	
+	@JsonProperty(value="precioVenta")
+	private Integer precioVenta;
+	
+	@JsonProperty(value="costosProduccion")
+	private Integer costosProduccion;
+	
+	@JsonProperty(value="tipoComidaProd")
+	private String tipoComidaProd;
+	
+	@JsonProperty(value="tiempoPreparacion")
+	private Integer tiempoPreparacion;
+	
+	public Producto(@JsonProperty(value="nombre")String pNom, @JsonProperty(value="categoria")Integer pCat,
+			@JsonProperty(value="precioVenta")Integer pVenta, @JsonProperty(value="costosProduccion")Integer pProd, 
+			@JsonProperty(value="tipoComidaProd")String ptipo, @JsonProperty(value="tiempoPreparacion")Integer ptiempo) {
+		super();
+		nombre=pNom;
+		categoria=pCat;
+		precioVenta=pVenta;
+		costosProduccion=pProd;
+		tipoComidaProd=ptipo;
+		tiempoPreparacion=ptiempo;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Integer getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Integer categoria) {
+		this.categoria = categoria;
+	}
+
+	public Integer getPrecioVenta() {
+		return precioVenta;
+	}
+
+	public void setPrecioVenta(Integer precioVenta) {
+		this.precioVenta = precioVenta;
+	}
+
+	public Integer getCostosDeProduccion() {
+		return costosProduccion;
+	}
+
+	public void setCostosDeProduccion(Integer costosDeProduccion) {
+		this.costosProduccion = costosDeProduccion;
+	}
+
+	public String getTipoComidaProd() {
+		return tipoComidaProd;
+	}
+
+	public void setTipoComidaProd(String tipoComidaProd) {
+		this.tipoComidaProd = tipoComidaProd;
+	}
+
+	public Integer getTiempoDePreparacion() {
+		return tiempoPreparacion;
+	}
+
+	public void setTiempoDePreparacion(Integer tiempoDePreparacion) {
+		this.tiempoPreparacion = tiempoDePreparacion;
+	}
 
 }
--- src/vos/Restaurante.java
+++ src/vos/Restaurante.java
@@ -1,5 +1,61 @@
 package vos;
 
+import org.codehaus.jackson.annotate.JsonProperty;
+
 public class Restaurante {
 
+	@JsonProperty(value="nombre")
+	private String nombre;
+	
+	@JsonProperty(value="descripcion")
+	private String descripcion;
+	
+	@JsonProperty(value="tipoComidaRest")
+	private String tipoComidaRest;
+	
+	@JsonProperty(value="paginaWeb")
+	private String paginaWeb;
+	
+	public Restaurante(@JsonProperty(value="nombre")String pNom, @JsonProperty(value="descripcion")String pDesc,
+			@JsonProperty(value="tipoComidaRest")String ptipo, @JsonProperty(value="paginaWeb")String ppag) 
+	{
+		super();
+		nombre=pNom;
+		descripcion=pDesc;
+		tipoComidaRest=ptipo;
+		paginaWeb= ppag;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public String getTipoComidaRest() {
+		return tipoComidaRest;
+	}
+
+	public void setTipoComidaRest(String tipoComidaRest) {
+		this.tipoComidaRest = tipoComidaRest;
+	}
+
+	public String getPaginaWeb() {
+		return paginaWeb;
+	}
+
+	public void setPaginaWeb(String paginaWeb) {
+		this.paginaWeb = paginaWeb;
+	}
+	
 }
