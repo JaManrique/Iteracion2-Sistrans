7d9409aa192289b35c3eb7093105d194cef3a502
Santiago Rodriguez Matiz <s.rodriguezm@uniandes.edu.co>
2017-10-02 23:37:33 -0500

    sql3

--- src/dao/DAOZona.java
+++ src/dao/DAOZona.java
@@ -68,4 +68,6 @@ public class DAOZona {
 		prepStmt.executeQuery();
 
 	}
+	
+	
 }
--- src/vosContainers/ZonaCompra.java
+++ src/vosContainers/ZonaCompra.java
@@ -1,5 +1,49 @@
 package vosContainers;
 
-public class ZonaCompra {
+import java.util.List;
 
+import org.codehaus.jackson.annotate.JsonProperty;
+
+import vos.CheckOut;
+
+public class ZonaCompra
+{
+	@JsonProperty(value="nombre")
+	private String nombre;
+	
+	@JsonProperty(value="capacidad")
+	private Integer capacidad;
+	
+	private List<CheckOut> checksO;
+	
+	public ZonaCompra(@JsonProperty(value="nombre")String pNom,@JsonProperty(value="capacidad")Integer ctrn) {
+		super();
+		nombre=pNom;
+		capacidad=ctrn;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Integer getCapacidad() {
+		return capacidad;
+	}
+
+	public void setCapacidad(Integer capacidad) {
+		this.capacidad = capacidad;
+	}
+
+	public List<CheckOut> getChecksO() {
+		return checksO;
+	}
+
+	public void setChecksO(List<CheckOut> checksO) {
+		this.checksO = checksO;
+	}
+	
 }
