1136d2f289d51825f032d03ff66b5fa2e4692dd7
Julián Manrique <ja.manrique@uniandes.edu.co>
2017-11-21 00:21:41 -0500

    AAAAAAAAAA

--- src/CSV/CSVgenerator.java
+++ src/CSV/CSVgenerator.java
@@ -9,14 +9,17 @@ import java.util.ArrayList;
 import java.util.Date;
 
 public class CSVgenerator {
-	
+
 	private File generatedCSV;
 	private File clientsCSV;
 	private File productsCSV;
+	private File menuCSV;
+	private File productoMenuCSV;
 	private File restaurantsCSV;
 	private File checkoutCSV;
 	private File producto_checkoutCSV;
-	
+	private File menu_checkoutCSV;
+
 	public CSVgenerator()
 	{
 		Date date = new Date();
@@ -24,10 +27,12 @@ public class CSVgenerator {
 		generatedCSV = new File("CSV/" + ds + "-users.csv");
 		clientsCSV = new File("CSV/" + ds + "-clientes.csv");
 		productsCSV = new File("CSV/" + ds + "-products.csv");
+		menuCSV = new File("CSV/" + ds + "-menu.csv");
 		restaurantsCSV = new File("CSV/" + ds + "-restaurants.csv");
 		checkoutCSV = new File("CSV/" + ds + "-checkouts.csv");
 		producto_checkoutCSV =  new File("CSV/" + ds + "-producto_checkout.csv");
-		
+		menu_checkoutCSV = new File("CSV/" + ds + "-menu_checkout.csv");
+
 		if(!generatedCSV.exists())
 		{
 			try {
@@ -39,6 +44,10 @@ public class CSVgenerator {
 				productsCSV.getParentFile().mkdirs();
 				productsCSV.createNewFile();
 				writeHeader("nombre,categoria,precioventa,costoproduccion,tipocomidaprod,tiempopreparacion\n", productsCSV);
+				//Menu CSV
+				menuCSV.getParentFile().mkdirs();
+				menuCSV.createNewFile();
+				writeHeader("nombre,categoria,precioventa,costoproduccion,tipocomidaprod,tiempopreparacion,restaurante_nombre\n",	menuCSV);
 				//Checkout CSV
 				checkoutCSV.getParentFile().mkdirs();
 				checkoutCSV.createNewFile();
@@ -47,11 +56,19 @@ public class CSVgenerator {
 				restaurantsCSV.getParentFile().mkdirs();
 				restaurantsCSV.createNewFile();
 				writeHeader("nombre,descripcion,timpodomidarest,paginaweb\n", restaurantsCSV);
-				
-			
-				
-				
-				
+				//PRODUCTO_CHECKOUT CSV
+				producto_checkoutCSV.getParentFile().mkdirs();
+				producto_checkoutCSV.createNewFile();
+				writeHeader("checkout_id,producto_nombre,cantidad_producto,restaurante_nombre\n", producto_checkoutCSV);
+				//MENU_CHECKOUT CSV
+				menu_checkoutCSV.getParentFile().mkdirs();
+				menu_checkoutCSV.createNewFile();
+				writeHeader("checkout_id,menu_nombre,cantidad_producto,restaurante_nombre\n", menu_checkoutCSV);
+
+
+
+
+
 				generateData();
 				System.out.println("Archivos creados en: " + generatedCSV.getAbsolutePath());
 			} catch (IOException e) 
@@ -73,10 +90,10 @@ public class CSVgenerator {
 				e.printStackTrace();
 			}
 		}
-		
+
 		System.out.println("Acabo bien :)");
 	}
-	
+
 	private void writeHeader(String string, File generatedCSV2) throws IOException
 	{
 		PrintWriter pw = new PrintWriter(generatedCSV2);
@@ -85,11 +102,11 @@ public class CSVgenerator {
 	}
 
 	private void generateData() throws IOException{
-		
+
 		long tiempoInicio = System.currentTimeMillis();
-		
+
 		String[] restaurantes = new String[10];
-		
+
 		restaurantes[0] = "Andrés Carne de Res";
 		restaurantes[1] = "Tostao";
 		restaurantes[2] = "Telepizza";
@@ -100,9 +117,9 @@ public class CSVgenerator {
 		restaurantes[7] = "Tao Calentao";
 		restaurantes[8] = "One Burrito";
 		restaurantes[9] = "Buffalo Wings";
-		
+
 		String[] productos = new String[15];
-		
+
 		productos[0] = "Carne asada";
 		productos[1] = "Coca cola";
 		productos[2] = "Sprite";
@@ -118,9 +135,28 @@ public class CSVgenerator {
 		productos[12] = "Burrito de una carne";
 		productos[13] = "Pizza Estándar";
 		productos[14] = "Malteada de Vainilla";
-		
+
+		String[] menus = new String[15];
+
+		menus[0] = "Menu ejecutivo";
+		menus[1] = "Menu corriente";
+		menus[2] = "Menu gorumet";
+		menus[3] = "Menu premium";
+		menus[4] = "Menu buffet";
+		menus[5] = "Menu agrandado";
+		menus[6] = "Menu principal";
+		menus[7] = "Menu de la sabana";
+		menus[8] = "Menu tapa arterias";
+		menus[9] = "Menu fitness";
+		menus[10] = "Menu tipico";
+		menus[11] = "Menu tradicional";
+		menus[12] = "Menu especial";
+		menus[13] = "Menu familiar";
+		menus[14] = "Menu infantil";
+
+
 		String[] usuarios = new String[15];
-		
+
 		usuarios[0] = "Juan";
 		usuarios[1] = "Gamer";
 		usuarios[2] = "Osito";
@@ -136,24 +172,24 @@ public class CSVgenerator {
 		usuarios[12] = "Choripan";
 		usuarios[13] = "Seneca";
 		usuarios[14] = "Matiz";
-		
+
 		String[] rol = new String[5];
-		
+
 		rol[0] = "cliente";
 		rol[1] = "admin restaurante";
 		rol[2] = "admin DB";
 		rol[3] = "gerente";
-		
+
 		String[] correo = new String[5];
-		
+
 		correo[0] = "@uniandes.edu.co";
 		correo[1] = "@hotmail.com";
 		correo[2] = "@mit.edu.co";
 		correo[3] = "@gmail.com";
 		correo[4] = "@outlook.com";
-		
+
 		String[] tiposComida = new String[10];
-		
+
 		tiposComida[0]= "Fitness";
 		tiposComida[9]= "Dietética";
 		tiposComida[8]= "Grasosa";
@@ -164,32 +200,32 @@ public class CSVgenerator {
 		tiposComida[3]= "Italiana";
 		tiposComida[2]= "Zhongguo cai";
 		tiposComida[1]= "Casera";
-		
+
 		String[] zonas = new String[5];
-		
+
 		zonas[0] = "Safari";
 		zonas[1] = "Bosque encantado";
 		zonas[2] = "Jungla oscura";
 		zonas[3] = "Desierto picante";
 		zonas[4] = "Nevados dulces";
-		
+
 		StringBuilder sb = new StringBuilder();
-		
-		StringBuilder sbClientes = new StringBuilder();
-		sbClientes.append("cliente\n");
-		
+
+		StringBuilder sbAux = new StringBuilder();
+		sbAux.append("cliente\n");
+
 		FileWriter pw = new FileWriter(generatedCSV, true);
-		PrintWriter pwClientes = new PrintWriter(clientsCSV);
-		
+		FileWriter pwAux = new FileWriter(clientsCSV, true);
+
 		//mucha MUCHA MUCHA info acá
 		ArrayList<String> clientes = new ArrayList<>();
-		
+
 		//Generar tabla usuarios-----------------
-		
+
 		try
 		{
 			int id = 0;
-			
+
 			for(id = 0; id < 1000000/*un millón*/; id++)
 			{
 				if(sb.length() > 3500)
@@ -199,18 +235,18 @@ public class CSVgenerator {
 					//Vaciar el String builder
 					sb.setLength(0);
 				}
-				
+
 				int r1 = (int) (Math.random()*5);
 				int r3 = (int) (Math.random()*15);
 				int r2 = (int) (Math.random()*15);
-				
+
 				String usr = usuarios[r3] + usuarios[r2];
-				
+
 				//Usuario
 				sb.append(usr + id + ",");
 				//Contraseña
 				sb.append("Pssw" + usr + id + ",");
-				
+
 				//Rol
 				if(r2 > 14)
 				{
@@ -227,20 +263,20 @@ public class CSVgenerator {
 				else
 				{
 					sb.append(rol[0] + ",");
-					sbClientes.append(usr + id + "\n");
+					sbAux.append(usr + id + "\n");
 					clientes.add(usr+id);
 					if(sb.length() > 500)
 					{
-						pwClientes.write(sbClientes.toString());
-						sbClientes.setLength(0);
+						pwAux.write(sbAux.toString());
+						sbAux.setLength(0);
 					}
 				}
-				
+
 				//Correo
-				
+
 				sb.append(usr + correo[r1] + "\n");				
 			}
-			
+
 			if(sb.length() > 0)
 			{
 				//Imprimir lo que lleva
@@ -248,22 +284,22 @@ public class CSVgenerator {
 				//Vaciar el String builder
 				sb.setLength(0);
 			}
-			
-			if(sbClientes.length() > 0)
+
+			if(sbAux.length() > 0)
 			{
-				pwClientes.print(sbClientes.toString());
-				sbClientes.setLength(0);
+				pwAux.write(sbAux.toString());
+				sbAux.setLength(0);
 			}
 		}
 		catch(IOException e)
 		{
 			pw.close();
-			pwClientes.close();
+			pwAux.close();
 			throw e;
 		}		
 		pw.close();
-		pwClientes.close();
-		
+		pwAux.close();
+
 		System.out.println("--------------------------TERMINO USER CSV------------------------");
 		System.out.println("t: " + (System.currentTimeMillis() - tiempoInicio) + "ms");
 
@@ -272,13 +308,13 @@ public class CSVgenerator {
 		//Reiniciar String Buffer
 		sb.setLength(0);
 		pw = new FileWriter(productsCSV, true);
-		
+
 		for(String prod : productos)
 		{
 			int r1 = (int) (Math.random()*150);
 			int r2 = 30 + (int) (Math.random()*250);
 			int r3 = (int) (Math.random()*10);
-			
+
 			sb.append(prod + ",");
 			sb.append(r1 + ",");
 			sb.append((r1*r2) + ",");
@@ -292,12 +328,42 @@ public class CSVgenerator {
 		pw.close();
 		System.out.println("--------------------------TERMINO PRODUCTOS CSV------------------------");
 		System.out.println("t: " + (System.currentTimeMillis() - tiempoInicio) + "ms");
-		
+
+
+		//Generar tabla MENU----------------------
+		tiempoInicio = System.currentTimeMillis();
+		//Reiniciar String Buffer
+		sb.setLength(0);
+		pw = new FileWriter(menuCSV, true);
+
+		for(String menu : menus)
+		{
+			int r1 = (int) (Math.random()*150);
+			int r2 = 30 + (int) (Math.random()*300);
+			int r3 = (int) (Math.random()*10);
+			int r4 = (int) (Math.random()*10);
+
+			sb.append(menu + ",");
+			sb.append(r1 + ",");
+			sb.append((r1*r2) + ",");
+			sb.append(r1*(r2-25) + ",");
+			sb.append(tiposComida[r3] + ",");
+			sb.append(((r1+r2+r3)/45) + ",");
+			sb.append(restaurantes[r4] + "\n");
+		}
+		//Escribir y reiniciar SB
+		pw.write(sb.toString());
+		sb.setLength(0);
+		pw.close();
+		System.out.println("--------------------------TERMINO PRODUCTOS CSV------------------------");
+		System.out.println("t: " + (System.currentTimeMillis() - tiempoInicio) + "ms");
+
+
 		//Generar CSV restaurantes ---------------------
-		
+
 		sb.setLength(0);
 		pw = new FileWriter(restaurantsCSV, true);
-		
+
 		for(String rest:restaurantes)
 		{
 			int r1 = (int) (Math.random()*10);
@@ -311,15 +377,16 @@ public class CSVgenerator {
 		pw.write(sb.toString());
 		pw.close();
 		sb.setLength(0);
-		
+
 		System.out.println("--------------------------TERMINO REST CSV------------------------");
 		System.out.println("t: " + (System.currentTimeMillis() - tiempoInicio) + "ms");
-		
+
 		//Generar CSV checkout ---------------------
-		
+
 		pw = new FileWriter(checkoutCSV, true);
+		SimpleDateFormat sf = new SimpleDateFormat("yyyyMMdd");
 		
-		for(int id = 0; id < 1000000 /*un millòn*/ ; id++)
+		for(int id = 1; id < 1000000 /*un millòn*/ ; id++)
 		{
 			if(sb.length() > 5000)
 			{
@@ -328,27 +395,103 @@ public class CSVgenerator {
 				//Vaciar el String builder
 				sb.setLength(0);
 			}
+
+			int rm =  (int) (Math.random()*11) + 1;
+			int rd =  (int) (Math.random()*26) + 1;
+			
+			String date = "";
+
+			if(rd > 9)
+				date += "" + rd + "/";
+			else
+				date += "0" + rd + "/";
+			
+			if(rm > 9)
+				date += "" + rm;
+			else
+				date += "0" + rm;
+			
+			date += "/2017";
 			
 			int r1 = (int) (Math.random()*2);
 			int r2 = (int) (Math.random()*5);
 			int r3 = (int) (Math.random()*clientes.size());
-			
+			int r4 = (int) (Math.random()*5);
+
 			sb.append(id + ",");
-			sb.append(zonas[r2] + ",");
-			sb.append(clientes.get(r3) + "\n");			
+			sb.append(r2 + ",");
+			sb.append(date + ",");
+			sb.append(zonas[r4] + ",");
+			sb.append(clientes.get(r3) + "\n");
 		}
-		
+
 		if(sb.length()>0)
 		{
 			pw.write(sb.toString());
 		}
 		sb.setLength(0);		
 		pw.close();
-		
+
 		System.out.println("--------------------------TERMINO CHECKOUT CSV------------------------");
-		System.out.println("t: " + (System.currentTimeMillis() - tiempoInicio) + "ms");		
+		System.out.println("t: " + (System.currentTimeMillis() - tiempoInicio) + "ms");
+
+
+		//Generar CSV PRODUCTO CHECKOUT / CSV MENU CHECKOUT ---------------
+
+		pwAux = new FileWriter(producto_checkoutCSV, true);
+		pw = new FileWriter(menu_checkoutCSV, true);
+		sbAux.setLength(0);
+
+		for(int id_chkout = 1; id_chkout<= 1000000; id_chkout++)
+		{
+			int r0 = (int) (Math.random()*5);
+
+			for(int i = 0; i < r0; i++)
+			{
+				int r1 = (int) (Math.random()*10);
+				int r2 = (int) (Math.random()*15);
+				int r3 = (int) (Math.random()*15);
+				int r4 = (int) (Math.random()*2);
+				int r5 = (int) (Math.random()*9)+1;
+
+				if(r4 == 1)//Insertar en producto checko
+				{
+					if(sbAux.length() > 3500)
+					{
+						//Imprimir lo que lleva
+						pwAux.write(sbAux.toString());
+						//Vaciar el String builder
+						sbAux.setLength(0);
+					}
+					sbAux.append(id_chkout + ",");
+					sbAux.append(productos[r2] + ",");
+					sbAux.append(r5 + ",");
+					sbAux.append(restaurantes[r1] + "\n");
+				}
+				else //inserter en menu checko
+				{
+					if(sb.length() > 3500)
+					{
+						//Imprimir lo que lleva
+						pw.write(sb.toString());
+						//Vaciar el String builder
+						sb.setLength(0);
+					}
+					sb.append(id_chkout + ",");
+					sb.append(menus[r2] + ",");
+					sb.append(r5 + ",");
+					sb.append(restaurantes[r1] + "\n");
+				}
+			}
+		}
+		pwAux.close();
+		pw.close();
+
+		System.out.println("--------------------------TERMINO PRODUCTO / MENU CHECKOUT CSV------------------------");
+		System.out.println("t: " + (System.currentTimeMillis() - tiempoInicio) + "ms");
+
 	}
-	
+
 	public static void main(String[] args) {
 		CSVgenerator csv = new CSVgenerator();		
 	}
